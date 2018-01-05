e26dea5980cb2718801433c620d960378e5560a5
Christos Lytras <christos.lytras@gmail.com>
2018-01-05 10:11:46 +0200

    Add EEPROM write at running mode (v1.1)
    Writes EEPROM data after 5 seconds the encoder has changed and when the program goes at running mode.
    Program version updated to 1.1

--- Program.cpp
+++ Program.cpp
@@ -4,7 +4,10 @@
  * Youtube Video: https://www.youtube.com/watch?v=ZAlxNNI-BVM
  * Author: Christos Lytras <christos.lytras@gmail.com>
  * Description: I'm using an older scanner as a box to create a UV PCB exposure box. I'll go through all steps of the hardware and software design emphasizing more to software logic.
- * Version: 1.0
+ * Version: 1.1
+ *
+ * 1.1
+ * - Change EEPROM write, to write only after 5 seconds the encoder has stopped and when the program goes at running mode
  *
  * */
 
@@ -190,6 +193,7 @@ void Program::doModeStart()
 		m_countdownSecs = m_secsCountdown;
 		m_countdownTimer = millis();
 		ledUVPower(true);
+		writeEEPROMData();
 	}
 }
 
